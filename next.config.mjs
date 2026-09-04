a37d18866317e8545d525e505e9f7921ef3b59f7
rohanvishwakarma2426 <iamrohan00126@gmail.com>
2026-09-04 17:31:53 +0530

    Skip ESLint during build, remove invalid config

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
-
 export default nextConfig;
